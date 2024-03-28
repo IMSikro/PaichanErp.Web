c47a57e8475bdf78e50f0cf23ed916402187b256
张兴智 <zhangxingzhi6118_xm.cicdi@chinaccs.cn>
2024-03-29 00:40:06 +0800

    doc:增加列控制页面

---
+++ src/views/system/tableColumns/index.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="tableColumns-container">
+		tableColumns
+	</div>
+</template>
+
+<script lang="ts" setup name="weChatUser">
+import { onMounted, reactive, ref } from 'vue';
+import { ElMessageBox, ElMessage } from 'element-plus';
+
+onMounted(async () => {
+	handleQuery();
+});
+
+// 查询操作
+const handleQuery = async () => {
+	state.loading = true;
+	let params = Object.assign(state.queryParams, state.tableParams);
+	var res = await getAPI(SysWechatUserApi).apiSysWechatUserPagePost(params);
+	state.weChatUserData = res.data.result?.items ?? [];
+	state.tableParams.total = res.data.result?.total;
+	state.loading = false;
+};
+</script>
