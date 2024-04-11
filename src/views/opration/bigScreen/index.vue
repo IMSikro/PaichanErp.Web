9512f42773aea33485b6cfe896c9a12282598f9c
张兴智 <nb77543@gmail.com>
2024-04-11 17:55:26 +0800

    docs: 修改样式2

--- src/views/opration/bigScreen/index.vue
+++ src/views/opration/bigScreen/index.vue
@@ -1,16 +1,35 @@
 <template>
 	<div class="bigScreen-container">
 		<div class="bigTitle">{{ state.orgData.name }} - 排产控制台</div>
+		<div class="content">
+			<el-collapse v-model="activeNames">
+				<el-collapse-item v-for="dt in state.deviceTypes" :key="dt.id" :name="dt.id">
+					<template #title>
+						<div class="title_bg"></div>
+						<el-text tag="b" style="margin-left: 3rem; color: white">{{ dt.typeName }}</el-text>
+					</template>
+					<DeviceList
+						:ref="
+							(e: any) => {
+								if (e) setDeviceListRef(e, dt);
+							}
+						"
+						:dt="dt"
+					/>
+				</el-collapse-item>
+			</el-collapse>
+		</div>
 	</div>
 </template>
 
 <script lang="ts" setup="" name="bigScreen">
 import { defineAsyncComponent, onMounted, reactive, ref } from 'vue';
 import { useRoute } from 'vue-router';
-import { SysUser, SysTenant } from '/@/api-services/models';
 import { getAPI } from '/@/utils/axios-utils';
-
+import { SysUser, SysTenant } from '/@/api-services/models';
+import { pageDeviceType } from '/@/api/main/deviceType';
 import { SysFileApi, SysOrgApi, SysUserApi, SysTenantApi } from '/@/api-services/api';
+import DeviceList from '/@/views/opration/subBig/deviceList.vue';
 
 const state = reactive({
 	currentUser: {} as SysUser,
@@ -19,10 +38,20 @@ const state = reactive({
 	deviceTypes: [] as any,
 });
 
+const activeNames = ref<string[]>([]);
 const getBaseInfo = async () => {
 	var res = await getAPI(SysOrgApi).apiSysOrgListGet(0);
 	var d = res.data.result ?? [];
 	state.orgData = d[0] ?? []; // 默认第一个树分支
+
+	var dtRes = await pageDeviceType({});
+	state.deviceTypes = dtRes.data.result?.items ?? [];
+	activeNames.value = state.deviceTypes.map((x) => x.id);
+};
+
+let deviceListRef = {};
+const setDeviceListRef = (e, dt) => {
+	deviceListRef[dt.id] = e;
 };
 
 onMounted(async () => {
@@ -34,6 +63,8 @@ onMounted(async () => {
 	display: flex;
 	color: #ffffff;
 	background-color: #000d3a;
+	padding: 1.5rem 3.25rem 0 3.25rem;
+	/* 顶部标题 */
 	.bigTitle {
 		display: flex;
 		justify-content: center;
@@ -41,7 +72,68 @@ onMounted(async () => {
 		font-size: 2rem;
 		width: 100%;
 		height: 6.0625rem;
-		background: url('../../../assets/bigScreen/bigTitle.png') no-repeat center center;
+		margin-bottom: 1.25rem;
+		background: url('../../../assets/bigScreen/bigTitleAll.png') no-repeat center center / 100% 100%;
 	}
+	.content {
+		width: 100%;
+		min-height: 37.5rem;
+		height: 100%;
+		position: relative;
+		.title_bg {
+			width: 1.875rem;
+			height: 1.875rem;
+			background: url('../../../assets/bigScreen/title_bg_top_left.png') no-repeat center center;
+			position: absolute;
+			left: 0;
+			top: 0;
+			z-index: 2;
+		}
+	}
+}
+
+:deep(.content) {
+	.el-collapse {
+		--el-collapse-border-color: transparent;
+		.el-collapse-item {
+			--el-collapse-border-color: transparent;
+			--el-collapse-header-bg-color: transparent;
+			--el-collapse-content-bg-color: transparent;
+		}
+	}
+}
+:deep(.el-collapse-item__header) {
+	width: 100%;
+	height: 42px;
+	border: 0.0625rem solid #01346d;
+	border-left: 0;
+	border-top: 0;
+	position: relative;
+	z-index: 1;
+	background: linear-gradient(to right, #048ef3, transparent);
+	.el-text {
+		font-size: 1rem;
+	}
+}
+:deep(.el-collapse-item__header::before) {
+	content: '';
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 0.0625rem;
+	background: linear-gradient(to right, transparent, #01346d);
+	z-index: 2;
+}
+:deep(.el-collapse-item__header::after) {
+	content: '';
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 0;
+	height: 0;
+	border-left: 2.1875rem solid transparent;
+	border-bottom: 3.4375rem solid #000d3a;
+	transform: rotate(180deg);
 }
 </style>
